ae376085f004fc091f3562bec8cd9c7b35dda778
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2022-08-10 09:09:24 +0300

    firebase, material

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'hobby-afa88',
+    appId: '1:823473123725:web:19716b7571c8e958db0dc4',
+    storageBucket: 'hobby-afa88.appspot.com',
+    apiKey: 'AIzaSyC_d8huMG6ZwZSBe0ErETBXoRUi9yicNsM',
+    authDomain: 'hobby-afa88.firebaseapp.com',
+    messagingSenderId: '823473123725',
+    measurementId: 'G-W1KWMMM37J',
+  },
   production: true
 };
